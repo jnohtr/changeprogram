b041ba0d670fb986cfdecb256081802debe12177
GBTC440007UR <GBTC440007UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-05-31 15:45:59 -0400

    first commit

---
+++ src/com/company/changeProgram.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class changeProgram {
+
+    public static void main(String[] args) {
+	// write your code here
+
+            int x;
+             double       z;
+            double y ;
+
+            x = 5;
+            y = 9.5;
+
+            z = x * y;
+
+            System.out.println("The product is " + z);
+        }
+
+}
